93dfd17a08a612f7c73d90cec2937062ab2c8dc8
Matthew <motmydro.2@gmail.com>
2025-07-21 12:19:15 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(function(){
+    $('.top-slider').slick({
+  dots: true,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  nextArrow: '<button type="button" class="slick-arrow slick-next"><img src="./img/next-arrow.svg" alt="next arrow"></button>',
+  prevArrow: '<button type="button" class="slick-arrow slick-prev"><img src="./img/prev-arrow.svg" alt="prev arrow"></button>',
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+      }
+    },
+
+    {
+      breakpoint: 640,
+      settings: {
+        arrows: false
+      }
+    }
+  ]
+});
+});
